5b8513a1564a466188907a5d30f4ab8210bea7d2
crobinson1985 <crobinson92828@gmail.com>
2025-05-01 15:10:14 -0400

    v0.1

---
+++ index.js
@@ -0,0 +1,76 @@
+const buttonColors = ['red', 'blue', 'green', 'yellow'];
+
+let userClickedPattern = [];
+let gamePattern = [];
+
+let level = 0;
+let started = false;
+
+let userChosenColor;
+let randomChosenColor;
+
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $('.' + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+  level++;
+  $('h1').text('Level ' + level);
+  userClickedPattern = [];
+  
+}
+
+function playSound (name) {
+  const colorSound = new Audio(`sounds/${name}.mp3`);
+  colorSound.play();
+}
+
+function animatePress (currentColor) {
+  let currentBtn = $('#' + currentColor);
+  currentBtn.addClass('pressed');
+  setTimeout(function() {
+    currentBtn.removeClass('pressed');
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {nextSequence();}, 1000);
+      
+    }
+    
+  } else {
+    let playSound = new Audio('sounds/wrong.mp3');
+    playSound.play();
+    started = false;
+    $('body').addClass('game-over');
+    setTimeout(function() {
+      $('body').removeClass('game-over');
+    }, 200);
+    $('h1').text('Game Over, Press Any Key to Restart');
+    userClickedPattern = [];
+    gamePattern = [];
+    level = 0;
+  }
+}
+
+$('.btn').click(function() {
+  userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+  
+ 
+});
+
+$('body').keydown(function() {
+  if(!started) {
+    nextSequence();
+    started = true;
+    
+  } 
+  
+});
